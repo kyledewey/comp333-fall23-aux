707e93d051a8e29e6c4c0c6ec5efad0fb61ed3e6
Kyle Dewey <kyle.dewey@csun.edu>
2023-08-30 12:45:50 -0700

    code up until the end of class

--- Test.java
+++ Test.java
@@ -9,24 +9,45 @@ public class Test {
     // public static String getDestinationFile(final String[] args) { ... }
 
     public static String getDestinationFileOrNull(final String[] args) { ... }
-    
-    public static int doComputation() { ... }
-    
-    public static void main(final String[] args) {
-        final String destinationFile = getDestinationFileOrNull(args);
-        final int result = doComputation();
 
+    public static FileOutputStream stream = null;
+    
+    // 1.) Should be a separate method to write output
+    // 2.) Files should only be opened once
+    public static void writeData(final String destinationFile,
+                                 final String data) {
         if (destinationFile == null) {
-            System.out.println(result);
+            System.out.println(data);
         } else {
             try {
-                final File file = new File(destinationFile);
-                final FileOutputStream stream = new FileOutputStream(file);
-                stream.writeln(result);
-                stream.close();
+                if (stream == null) {
+                    final File file = new File(destinationFile);
+                    stream = new FileOutputStream(file, true);
+                }
+                stream.writeln(data);
             } catch (final Exception e) {
                 System.out.println("error: " + e.getMessage());
             }
+        }
+    }
+    
+    // Takes days
+    public static int doComputation(final String destinationFile) {
+        // do some work...
+        writeData(destinationFile, "still working");
+
+        // do some more work here...
+        writeData(destinationFile, "yep, still working");
+    }
+    
+    public static void main(final String[] args) {
+        final String destinationFile = getDestinationFileOrNull(args);
+        final int result = doComputation(destinationFile);
+
+        writeData(destinationFile, Integer.toString(result));
+
+        if (stream != null) {
+            stream.close();
         }
     }
 }
