159b3f9f10e89047e650f6a5b5506ae2876e76d7
Kyle Dewey <kyle.dewey@csun.edu>
2023-10-27 07:34:17 -0700

    code from class

--- in_class_section/javascript/objects.js
+++ in_class_section/javascript/objects.js
@@ -9,14 +9,33 @@
 // - Objects all the way down
 // - Can inherit from a prototype (just another object)
 
-function getArea(rec) {
-    return rec.width * rec.height;
-}
+// let temp = {
+//     'getArea': function() {
+//         return this.width * this.height;
+//     }
+// };
+
+// Rectangle = function(w, h) { ... }
+
+Rectangle.prototype.getArea = function() {
+    return this.width * this.height;
+};
 
 // Constructor
 function Rectangle(w, h) {
-    // this = {}
+    // this = {};
+    // this.__proto__ = Rectangle.prototype;    
     this.width = w;
     this.height = h;
     // return this;
 }
+
+// Calls:
+// 1.) Function call - f(1, 2)
+// 2.) Constructor call - new f(3, 4)
+//     this = {};
+//     ...body of constructor...
+//     return this;
+// 3.) Method call - a.f(5, 6)
+//     this = a;
+//     ...body of method...
