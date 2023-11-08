d136564cb4a66bf8edddf79e0010a2f843e4b3e7
Kyle Dewey <kyle.dewey@csun.edu>
2023-11-08 12:07:01 -0800

    code from class

---
+++ online_section/swift/intro.swift
@@ -0,0 +1,19 @@
+// public static int foo(int x) { ... }
+func fooWithExplicitExternal(param x: Int) -> Int {
+    return x;
+}
+
+func fooWithNoExternal(_ x: Int) -> Int {
+    return x;
+}
+
+func fooWithImplicitExternal(x: Int) -> Int {
+    return x;
+}
+
+print("Hello");
+let temp1 = fooWithExplicitExternal(param: 3);
+let temp2 = fooWithNoExternal(4);
+let temp3 = fooWithImplicitExternal(x: 5);
+print(temp1 + temp2 + temp3);
+
